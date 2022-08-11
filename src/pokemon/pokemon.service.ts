0364433f4de4e4111182dd2b060f4b9f523250d2
Jose Domenico <domenicodiaz.joseangel@gmail.com>
2022-08-11 16:30:50 -0700

    [ADD]: Se agrego seed para llenar pokemons desde el api de pokeapi

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -6,7 +6,6 @@ import {
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { isValidObjectId, Model } from 'mongoose';
-import { withLatestFrom } from 'rxjs';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
